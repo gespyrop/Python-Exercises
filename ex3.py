b9fa7eb2ff837892c92051f7f3b0ccc1341ce90e
Γιώργος Σπυρόπουλος <gespyrop@hotmail.gr>
2018-01-25 00:11:39 +0200

    rot13 function added

--- ex3.py
+++ ex3.py
@@ -1,3 +1,16 @@
 lower = [chr(i) for i in range(ord("a"), ord("z") + 1)]
 upper = [chr(i) for i in range(ord("A"), ord("Z") + 1)]
 alphabet = lower + upper
+
+def rot13(character):
+    if character == character.upper():
+        if ord(character) + 13 <= ord(upper[-1]):
+            return chr(ord(character) + 13)
+        else:
+            return chr(ord(character) + 13 - len(upper))
+    else:
+        if ord(character) + 13 <= ord(lower[-1]):
+            return chr(ord(character) + 13)
+        else:
+            return chr(ord(character) + 13 - len(lower))
+
