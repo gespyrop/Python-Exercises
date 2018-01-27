8da6bccca1a43c14dd81e430a86dc4f2756a0a65
Γιώργος Σπυρόπουλος <gespyrop@hotmail.gr>
2018-01-27 21:14:12 +0200

    Final

--- ex7.py
+++ ex7.py
@@ -3,11 +3,13 @@ import datetime
 today = datetime.datetime.now()
 day = today
 
-Monday8 = 0
+daysOfTheWeek = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+
+s = 0
 
 while day.year <= today.year + 10:
-    if day.day == 8 and day.weekday() == 0:
-        Monday8 += 1
     day += datetime.timedelta(days = 1)
+    if day.day == today.day and day.weekday() == today.weekday():
+        s += 1
 
-print "\nIn the next 10 years,%d Mondays are the 8th of the month they belong to!" % Monday8
+print "\nIn the next 10 years,", str(s), daysOfTheWeek[today.weekday()] + "s are the " + str(today.day), "of the month they belong to!"
