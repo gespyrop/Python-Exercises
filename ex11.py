e0e852c99244f9eabce20bacd8f43f382ce89659
Γιώργος Σπυρόπουλος <gespyrop@hotmail.gr>
2018-02-10 00:02:15 +0200

    Square created and printed

--- ex11.py
+++ ex11.py
@@ -2,3 +2,13 @@ import random
 
 alphabet = [chr(i) for i in range(ord("A"),ord("Z") + 1)]
 
+rows = []
+
+for i in range(100):
+    row = ""
+    for j in range(100):
+        row += alphabet[random.randrange(len(alphabet))]
+    rows += [row]
+
+for row in rows:
+    print row
