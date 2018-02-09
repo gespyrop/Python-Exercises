fb196a1f4611157f6f65013e655f8dc2a83cdd91
Γιώργος Σπυρόπουλος <gespyrop@hotmail.gr>
2018-02-10 00:10:15 +0200

    Added the text file containing the words and created a list for columns

--- ex11.py
+++ ex11.py
@@ -3,6 +3,7 @@ import random
 alphabet = [chr(i) for i in range(ord("A"),ord("Z") + 1)]
 
 rows = []
+columns = []
 
 for i in range(100):
     row = ""
@@ -10,5 +11,16 @@ for i in range(100):
         row += alphabet[random.randrange(len(alphabet))]
     rows += [row]
 
+for i in range(100):
+    column = ""
+    for row in rows:
+        column += row[i]
+    columns.append(column)
+
+with open("words.txt", "r") as text:
+    words = [line.rstrip().upper() for line in text]
+
 for row in rows:
     print row
+
+print words
