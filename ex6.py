dc38a989244ada0490e799c60820ff186a162a4f
Γιώργος Σπυρόπουλος <gespyrop@hotmail.gr>
2018-02-09 17:40:43 +0200

    Days added

--- ex6.py
+++ ex6.py
@@ -12,3 +12,11 @@ months = {1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "Jun
 day = datetime.date(year, month, 1)
 
 print "\n\t\t    ", months[month], year, "\n\n\nS\tM\tT\tW\tT\tF\tS\n" + 51 * "_"
+
+while day.month == month:
+    print day.day, "\t",
+    if day.weekday() == 5:
+            print "\n"
+    day += datetime.timedelta(days = 1)
+
+print "\n"
