91a88468e3ce2f3d9ad271928a4387ea3919dd9f
Γιώργος Σπυρόπουλος <gespyrop@hotmail.gr>
2018-02-11 14:28:42 +0200

    Dictionary replaced with tuple as it was indexed from 0 to 6

--- ex7.py
+++ ex7.py
@@ -3,7 +3,7 @@ import datetime
 today = datetime.datetime.now()
 day = datetime.date(today.year + 1, 1, 1)
 
-daysOfTheWeek = {0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}
+daysOfTheWeek = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
 
 s = 0
 
